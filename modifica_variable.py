f8212f98aeaf76eb68abb0ea9dead858ce09a0f2
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 14:49:29 -0400

    Aprendiendo a modificar variables

---
+++ modifica_variable.py
@@ -0,0 +1,13 @@
+nombre = "Superman"
+nivel_energia = 85
+tiene_capa = True
+
+print("===Identidad Original=== \nNombre : ",nombre, "\nNivelenergia: ",nivel_energia, "\nTiene capa: ",tiene_capa)
+
+# Ahora vamos a mpdificar las variables
+print("\n===Nueva Identidad===")
+nombre = "Clark Kent"
+nivel_energia = 0
+tiene_capa = False
+
+print("\nNombre : ",nombre, "\nNivelenergia: ",nivel_energia, "\nTiene capa: ",tiene_capa)
