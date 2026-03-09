7eb1f35f035a77a0eca25d3eaf144605279ed09a
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 14:36:14 -0400

    Agregar programa para mostrar la ficha secreta de un superhéroe

---
+++ tipos_datos.py
@@ -0,0 +1,27 @@
+# Programa: Ficha secreta del superheroe
+
+nombre = "Batman"
+poder_principal = "Inteligencia"
+
+edad = 27
+misiones_completadas = 89
+
+nivel_energia = 99.8
+
+tiene_capa = True
+es_villano = False
+
+# Nonetypr
+compania_actual = None
+
+print(f"""
+      Ficha Secreta del Superhéroe:
+      Nombre: {nombre}
+      Poder Principal: {poder_principal}
+      Edad: {edad}
+      Misiones Completadas: {misiones_completadas}
+      Nivel Energia: {nivel_energia}
+      Tiene Capa: {tiene_capa}
+      Es Villano: {es_villano}
+      Compañía Actual: {compania_actual}
+      """)
