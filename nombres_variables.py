8a4859a377b048ad9dcff0f109f0162756388640
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 15:03:52 -0400

    aprendiendo reglas de python

---
+++ nombres_variables.py
@@ -0,0 +1,17 @@
+# Programa Registro explorador espacial
+
+nombre_explorador = "Lina Vega"
+planeta_origen= "Marte"
+edad_explorador = 29
+misiones_completadas = 4
+nivel_energia = 87.5
+
+# no usar palabras reservadas como nobre de variable
+NIVEL_ENERGIA = 100.00
+# Aunque esta es una comstante por convencion se escribe en mayuscula, pero no es una constante real, ya que puede ser modificada
+
+print("Nombre del Explorador: ",nombre_explorador)
+print("Planeta de Origen: ",planeta_origen)
+print("Edad: ",edad_explorador)
+print("Misiones Completadas: ",misiones_completadas)
+print("Nivel de Energia: ",nivel_energia)
