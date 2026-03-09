f3fc77924f0c6bd7be0b2845c5534dc97e3edc37
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 15:20:31 -0400

    Practica de modificacion de datos (Repaso)

---
+++ reserva_hoteles.py
@@ -0,0 +1,19 @@
+nombre_cliente = "Laura Martinez"
+dias_estancia = 5
+tarifa_diaria = 1200.00
+vista_al_mar = True
+
+print("Cliente: ",nombre_cliente)
+print("Dias de Estancia: ",dias_estancia)
+print("Tarifa Diaria: ",tarifa_diaria)
+print("Vista al Mar: ",vista_al_mar)
+
+
+dias_estancia = 7
+tarifa_diaria = 1000.00
+vista_al_mar = False
+
+print("Cliente: ",nombre_cliente)
+print("Dias de Estancia: ",dias_estancia)
+print("Tarifa Diaria: ",tarifa_diaria)
+print("Vista al Mar: ",vista_al_mar)
