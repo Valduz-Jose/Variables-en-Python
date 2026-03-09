c9094c5d21b4ce4b301d312b54f9574473587b9c
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 14:21:19 -0400

    Usando variables para almacenar datos personales

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Programa para almacenar informacion personal
+
+# Guardar los datos en variables
+nombre = "Alejandro"
+edad = "27"
+pais = "Venezuela"
+frase_favorita = "Nunca dejes de aprender"
+# Mostramos la info en consola
+
+print("Nombre: ",nombre)
+print("Edad: ",edad)
+print("Pais: ",pais)
+print("Frase Favorita: ",frase_favorita)
